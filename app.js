d615d5e492e3b7ac4c7cfcad9646177c5cfd0dcc
Nikolla2502 <nikolla@wanadoo.fr>
2021-11-24 08:49:10 +0100

    nodemon express server.js app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = express();
+
+
+
+
+
+
+app.use((req, res) => {
+    res.json({ message: 'Votre requête a bien été reçue !' });
+    });
+
+
+
+
+
+
+
+
+
+module.exports = app;
