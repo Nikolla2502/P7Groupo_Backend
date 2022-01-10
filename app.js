39dfc3f2304170fc458bdabe5bd59cd54c13c770
Nikolla2502 <nikolla@wanadoo.fr>
2022-01-10 10:56:56 +0700

    creation fichier mysql

--- app.js
+++ app.js
@@ -3,29 +3,18 @@ const app = express();
 app.use(express.json()); // bodyparser deprecated
 
 const path = require('path');
-const helmet = require('helmet');     // prevention contre les attaques XSS dans les HTTP headers
-const mysql = require('mysql');
+
+// prevention contre les attaques XSS dans les HTTP headers
+const helmet = require('helmet');     
+
 
 // liaison fichier environnemet
 require('dotenv').config()
 
+// connexion mySQL
+require ('./mySQL.js')
 
-// connection MySQL
-const db = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    
-});
-
-db.connect(function(err) {
-    if (err) 
-        throw err;
-    console.log("Connecté à la base de données MySQL!");
-  });
-
-
+// Cors
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
---
+++ mySQL.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql');
+
+// connection MySQL
+const db = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    
+});
+
+db.connect(function(err) {
+    if (err) 
+        throw err;
+    console.log("Connecté à la base de données MySQL!");
+  });
+
+
+  module.exports = db;
