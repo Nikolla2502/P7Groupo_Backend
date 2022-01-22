b8c165f439aff933f0de2b06744fd5b5070377fe
Nikolla2502 <nikolla@wanadoo.fr>
2022-01-22 16:44:40 +0700

    modif insert comment

--- controllers/threadCtrl.js
+++ controllers/threadCtrl.js
@@ -1,19 +1,52 @@
+const db = require('../mySQL');
+
 // creer un post
 exports.createPost = (req, res, next) => {
+    const sqlCreatePost = "INSERT INTO 'subject' ('date_modify', 'id_user', 'title', 'id_type', 'id_category')"; // Penser à renseigner les valeurs
+    db.query(sqlCreatePost, [], (err, result) => {
+        // Si INSERT ok, INSERT contenu dans comments, ne pas oublier de récupérer l'id du sujet (variable result.insertId)
+    })
 
 };
 
 // recuperer tous les posts
 exports.getAllPosts = (req, res, next) => {
+    const sqlGetAllPosts = "SELECT * FROM subject"
+    db.query(sqlGetAllPosts,[],(err,result) => {
+        if (result) {
+            res.status(201).json({result})
+        } else {
+           return res.status(400).json({error})
+        }
+    })
 
 };
 
-// modifier un post
-exports.modifyPost = (req, res, next) => {
+// ajout commentaire sur un post
+exports.addComment = (req, res, next) => {
+    const sqlAddComment = "INSERT INTO comments ('date_create', 'comment', 'id_user') WHERE id_subject=?"; 
+    db.query(sqlAddComment,[],(err,result) => {
+        if(result) {
+            res.status(201).json({result})
+        } else {
+            return res.status(400).json({message:' Publication non trouvé '})
+        }
+    })
 
 };
 
+// MODIFIER commentaire sur un post
+exports.addComment = (req, res, next) => {
+    const sqlAddComment = "UPDATE INTO comments ('date_modify', 'comment', 'id_user') WHERE id_subject=?"; 
+    db.query(sqlAddComment,[],(err,result) => {
+        if(result) {
+            res.status(201).json({result})
+        } else {
+            return res.status(400).json({message:' Publication non trouvé '})
+        }
+    })
 
+};
 
 // user like dislike
 exports.userLikePost = (req, res, next) => {
@@ -21,7 +54,7 @@ exports.userLikePost = (req, res, next) => {
 };
 
 
-// admin privilege
+// delete post admin privilege
 exports.deletePost = (req, res, next) => {
-
+    const sqldeletePost = "SELECT * FROM 'subject' WHERE id_subject=?"
 };
--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -7,58 +7,46 @@ require('dotenv').config();
 
  // creation d'un nouvel utilisateur
  exports.signup = (req, res, next) => {
-    const sqlCreateUser = "INSERT INTO `user` (`pseudo`,email`,`password`) VALUES (?, ?, ?)";
+    const sqlCreateUser = "INSERT INTO `user` (`pseudo`,`email`,`password`) VALUES (?, ?, ?)";
     bcrypt.hash(req.body.password, 10)      // on hash le mot de passe 10 fois
-    .then(hash => {
-        db.query(sqlCreateUser, [req.body.pseudo, req.body.email, hash], (err,result) => {
-            //Requete terminée, traiter la reponse
-        })
-        .catch(error => res.status(500).json({ error }));
-    });
+        .then(hash => {
+            db.query(sqlCreateUser, [req.body.pseudo, req.body.email, hash], (err, result) => {
+                if (result) {
+                    res.status(201).json({ message: 'Utilisateur créé'})
+                } else {
+                    return res.status(400).json({err});
+                }
+            })
+            .catch(error => res.status(500).json({ error }));
+        });
 };
-// exports.signup = (req, res, next) => {           
-//     bcrypt.hash(req.body.password, 10)      // on hash le mot de passe 10 fois
-//     .then(hash => {
-//         const user = new User({
-//             email: req.body.email,
-//             password: hash
-//         });
-//         user.save()
-//             .then(()=> res.status(201).json({message: 'Utilisateur créé  !' }))
-//             .catch(error => res.status(400).json({ error: 'Mot de passe non conforme !' }))
-//     })
-//     .catch(error => res.status(500).json({ error }));
-// };
+
 
 // connexion d'un utilisateur existant
 exports.login = (req, res, next) => {
-    const sqlLoginUser = "SELECT * FROM `user` WHERE email=?"
+    const sqlLoginUser = "SELECT * FROM `user` WHERE email=?";
+
     db.query(sqlLoginUser, [], (err,result) => {
+        if (err) {
+            return res.status(400).json({ message: 'Utilisateur non trouvé !'})
+        } else {
+            bcrypt.compare(req.body.password, user.password)         // bcrypt compare le password de l'utilisateur avec le password stocke dans la bdd
+            .then(valid => {
+                if (!valid) {
+                    return res.status(401).json({error: 'Mot de passe Incorrect'});
+                }
+                res.status(200).json({
+                    userId: user._id,
+                    token:jwt.sign(
+                        { userId: user._id },
+                        process.env.TOKEN,
+                        {expiresIn: '24h'}
+                    )}
+                );
+            })
+            .catch(err => res.status(500).json({error}))
+        };
+    })
+}
 
-    });
-};
 
-
-// exports.login = (req, res, next) => {
-//     User.findOne({ email: req.body.email })
-//     .then(user => {
-//         if (!user) {
-//             return res.status(401).json({error: 'Utilisateur non trouvé'});
-//         }
-//         bcrypt.compare(req.body.password, user.password)         // bcrypt compare le password de l'utilisateur avec le password stocke dans la bdd
-//         .then(valid => {
-//             if (!valid) {
-//                 return res.status(401).json({error: 'Mot de passe Incorrect'});
-//             }
-//             res.status(200).json({
-//                 userId: user._id,
-//                 token:jwt.sign({ userId: user._id },
-//                 process.env.TOKEN,
-//                 { expiresIn: '24h'}
-//                 )
-//             });
-//         })
-//         .catch(error => res.status(500).json({error}));
-//     })
-//     .catch(error => res.status(500).json({error}));
-// };
